ea59392f409dfde39b2a27f6de370821556832c3
Carter-eng <52756780+Carter-eng@users.noreply.github.com>
2022-02-15 16:17:03 -0500

    Add files via upload

---
+++ listener_accumulator.py
@@ -0,0 +1,33 @@
+import rospy
+from std_msgs.msg import String
+import threading
+class listen_accumulator:
+	def __init__(self):
+		rospy.init_node('listener_accumulator', anonymous=True)
+
+		self.listening=threading.Thread(target=self.listen,)
+		self.listening.start()
+		self.chatter_data = ''
+		self.pub = rospy.Publisher('chatter_repeated',String, queue_size=10)
+		self.rate = rospy.Rate(.333)
+		msg = String()
+		while not rospy.is_shutdown():
+			msg.data = self.chatter_data
+			rospy.loginfo(msg.data)
+			self.pub.publish(msg)
+			self.rate.sleep()
+
+
+
+	def listen(self):
+		sub = rospy.Subscriber('chatter',String,self.callback)
+		rospy.spin()
+
+	def callback(self,data):
+		self.chatter_data = data
+
+if __name__ == '__main__':
+	try:
+		x=listen_accumulator()
+	finally:
+		pass
